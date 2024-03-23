d399b34b4e4a5a47c47b6402a3a179e08fcd347b
gwc2000 <gwc2000@hw.ac.uk>
2024-03-23 14:30:12 +0000

    Multiprocessing interupts work, just need to clean up the intergration with dm

--- app.py
+++ app.py
@@ -18,57 +18,58 @@ import TTS_Module as tts_mod
 from ctypes import c_bool, c_wchar_p
 
 if __name__ == "__main__":
-    # eel.init('web') ???
+    eel.init('web')
     mp.set_start_method('spawn')
     
     rm = RestaurantManager()
     global user_speaking_event
     user_speaking_event = mp.Event()
-    global mic_input 
-    mic_input = mp.Value(c_wchar_p, lock=True)
+    global main_con, mic_con
+    main_con, mic_con = mp.Pipe()
     user_speaking_event.clear()
-    mic_input.acquire()
-    mic_input.value = ""
-    mic_input.release()
-    print("Speaking flag ", user_speaking_event.is_set())
-    print("Mic input ", mic_input.value)
+    global engine
+    engine = tts_mod.create_tts_engine()
+
+    # print("Speaking flag ", user_speaking_event.is_set())
 
 
 
 # initiate a clarification request from GPT for the relevant slot.
 def clar_request(slot):
-    global mic_input
+    global engine
+    global mic_con
+    global main_con
     global user_speaking_event
     #send request to GPT, then update the chat text and speak the response
     ans = rm.sendClarification(slot) #
     eel.updatechattext(ans) 
     # p = multiprocessing.Process(target=speak, args=(ans,)) You have set this up twice??
-    setup_speak_process(ans)
-
+    setup_listen_process(mic_con, user_speaking_event)
+    # setup_speak_process(ans)
+    engine.say(ans)
+    engine.runAndWait()
+    # user_speaking_event.wait()
+    curr_Utt = main_con.recv()
+    user_speaking_event.clear()
     # get the user's response, send to GPT, convert the gpt response to our python dict.
-    setup_listen_process(mic_input, user_speaking_event)
-    user_speaking_event.wait()
-    gpt_output = rm.sendSlotPrompt(mic_input.value)
+    gpt_output = rm.sendSlotPrompt(curr_Utt)
     
-    print(gpt_output)
+    print("GPT's response is: ", gpt_output)
     gpt_dict = rm.convert_stringtodict(gpt_output)
+
+    print("Current slots are: ", rm.restaurant_slots)
 
     #update the slots with the new information
     rm.updateSlots(gpt_dict)
-    print("Slots are: ", rm.restaurant_slots)
-    print("response is: ", gpt_output)
+    print("Updated slots are: ", rm.restaurant_slots)
 
-
-
-def thread_killer(sendKillRequest=False):
-    return sendKillRequest
 
 # Function to handle microphone input
-def setup_listen_process(mi, sf):
-    p = mp.Process(target=listen_for_user_input, args=[mi, sf], daemon=True)
+def setup_listen_process(con_to_main, user_speaking_event):
+    p = mp.Process(target=listen_for_user_input, args=(con_to_main, user_speaking_event), daemon=True)
     p.start()
         
-def listen_for_user_input(mic_input, user_speaking_event):
+def listen_for_user_input(con_to_main, user_speaking_event):
     while True:
         # Listen for microphone input
         curr_input = microphone_ASR.set_up()
@@ -78,60 +79,85 @@ def listen_for_user_input(mic_input, user_speaking_event):
         if curr_input:
             # If there is speech input, set the speaking flag to True
             print("User input: ", curr_input)
-            mic_input.acquire()
-            try:
-                mic_input.value = curr_input
-            finally:
-                mic_input.release()
             
+            con_to_main.send(curr_input)
             user_speaking_event.set()
             # Update the chat text with user input
-            eel.updatechattext("Speaker: " + curr_input)
+            print("User speaking event is set: aaaa", user_speaking_event.is_set())
             break
 
-# If user is speaking before the tts is speaking, then tts will not say anything (?)
+# If user is speaking before the tts is speaking, then tts will not say anything (?)d
+def interrupt_speaker(name, location, length):
+    global main_con
+    print ('word', name, location, length)
+    print("User speaking event is set: ", user_speaking_event.is_set())
+    print("Main con is: ", main_con.poll())
+    if main_con.poll():
+        print("User is speaking, please wait.")
+        engine.stop()
+        # engine.say("User is speaking, please wait.")
+        # engine.runAndWait()
+        # print("2User is speaking, please wait2.")
+
 def setup_speak_process(msg):
-    p = mp.Process(target=speak, args=( msg,), daemon=True)
-    p.start()
-    while p.is_alive():
-        if user_speaking_event.is_set(): 
-            p.terminate()
-        else:
-            continue
-    p.join()
+    global engine
+    # p = mp.Process(target=speak, args=( msg,), daemon=True)
+    engine.connect('started-word', interrupt_speaker)
+    engine.say(msg,"msg")
+    engine.runAndWait()
+    # p.start()
+    # print(p.name)
+    # while p.is_alive():
+    #     if user_speaking_event.is_set(): 
+    #         print("User is speaking, terminating: " + p.name)
+    #         p.terminate()
+    #     else:
+    #         continue
+    # p.join()
 
 # TTS speaker function (for multi-threading)
-def speak(text):
-    engine = tts_mod.create_tts_engine()
+def speak(text, engine):
+    # engine = tts_mod.create_tts_engine()
     engine.say(text)
     engine.runAndWait()
 
 @eel.expose
 def append_to_chattext():
-    global mic_input
     global user_speaking_event
+    global main_con
+    global mic_con
     start_text = "Can I help you book a restaurant? The fitness grand-pacer test is a multistage arobic test, that progressively gets more difficult as you continue."
     eel.updatechattext("ChatGPT: " + start_text)
-    setup_listen_process(mic_input, user_speaking_event)
+    setup_listen_process(mic_con, user_speaking_event)
     setup_speak_process(start_text)
-    start_app()
+    find_info()
     ans = "Convo Finished" # all slots have been found, terminate conversation.
     eel.updatechattext(ans)
     print("Convo Finished")
+    return
 
-def start_app():
-    global mic_input
+def find_info():
+    global engine
+    global main_con
+    global mic_con
     global user_speaking_event
-    user_speaking_event.wait()
+    print("starting wait")
+    curr_Utt = main_con.recv()
+    eel.updatechattext("Speaker: " + curr_Utt)
+    user_speaking_event.clear()
     #Send uterance to GPT, convert the response to our python dict, and update
-    mic_input.acquire()
-    try:
-        gpt_output = rm.sendSlotPrompt(mic_input.value)
-    finally:
-        mic_input.release()
+    gpt_output = rm.sendSlotPrompt(curr_Utt)
         
-    gpt_dict = rm.convert_stringtodict(gpt_output)
-    rm.updateSlots(gpt_dict)
+    
+    try:
+        gpt_dict = rm.convert_stringtodict(gpt_output)
+    except ValueError: 
+        print("ValueError found... retrying...")
+        gpt_output = rm.sendSlotPrompt(curr_Utt)
+        print(f'New GPT Response after ValueError:\n{gpt_output}\n')
+        gpt_dict = rm.convert_stringtodict(gpt_output) #retry...
+        print("Slots after error: ", rm.restaurant_slots)
+        #clar_request(currKey)    rm.updateSlots(gpt_dict)
 
     print("Slots beginning: ", rm.restaurant_slots)
     keys = rm.check_empty_slots()
@@ -147,16 +173,15 @@ def start_app():
             #GPT requests slot from user
             slot_request =  rm.askForSlot(currKey)
             eel.updatechattext("ChatGPT: "+slot_request)
-            setup_listen_process(mic_input, user_speaking_event)
-            setup_speak_process(slot_request)
+            setup_listen_process(mic_con, user_speaking_event)
+            # setup_speak_process(slot_request)
+            engine.say(slot_request)
+            engine.runAndWait()
+            curr_Utt = main_con.recv()
+            user_speaking_event.clear()
         
-            # print(f"ASR took {end_mic_time - start_mic_time} seconds")
-            # print(f"Chat text took {end_chattext_time - end_mic_time} seconds")
-            # print(f"Speaker took {end_chattext_time - start_speaker_time} seconds")
 
-            mic_input.acquire()
-            gpt_output = rm.sendSlotPrompt(mic_input.value)
-            mic_input.release()
+            gpt_output = rm.sendSlotPrompt(curr_Utt)
             print(f'GPT Response:\n{gpt_output}\n')
 
             # Sometimes GPT produces a dict / JSON that doesn't fit with our expected format.
@@ -167,9 +192,7 @@ def start_app():
                 gpt_dict = rm.convert_stringtodict(gpt_output)
             except ValueError: 
                 print("ValueError found... retrying...")
-                mic_input.acquire()
-                gpt_output = rm.sendSlotPrompt(mic_input.value)
-                mic_input.release()
+                gpt_output = rm.sendSlotPrompt(curr_Utt)
                 print(f'New GPT Response after ValueError:\n{gpt_output}\n')
                 gpt_dict = rm.convert_stringtodict(gpt_output) #retry...
                 print("Slots after error: ", rm.restaurant_slots)
@@ -189,11 +212,6 @@ def start_app():
             print("Keys in loop are: ", keys)
     return 
    
-    # p = multiprocessing.Process(target=speak, args=(ans,))
-    #global flag_tts
-    # flag_tts = False
-    # setup_speak_process(p)
-    
     #this resets the conversation once it is finished. 
     #Alternative is to rest when user presses speak button
     # rm.empty_slots()# = RestaurantManager()
